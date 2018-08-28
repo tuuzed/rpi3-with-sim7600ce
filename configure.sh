621efbf113043bfa4f4322ae21ac46606b64200b
tuuzed <liuyonghui.job@gmail.com>
2018-08-28 20:51:51 +0800

    update

--- configure.sh
+++ configure.sh
@@ -10,17 +10,21 @@ export CONFIG_SPEED_UP=6
 # 内核版本
 export KERNEL_VERSION=rpi-4.9.y-stable
 
+export TEMP_PATH=$CONTEXT_PATH/temp
+
+export CACHE_PATH=$CONTEXT_PATH/.cache
+
 # 系统内核源码路径
-export LINUX_PATH=$CONTEXT_PATH/temp/linux
+export LINUX_PATH=$TEMP_PATH/linux
 
 # 交叉编译工具路径
-export TOOLS_PATH=$CONTEXT_PATH/temp/tools
+export TOOLS_PATH=$TEMP_PATH/tools
 
 # 升级补丁
 export UPGRADE_PATCH=UpgradePatch
 
 # 编译好的升级补丁路径
-export UPGRADE_PATCH_PATH=$CONTEXT_PATH/temp/$UPGRADE_PATCH
+export UPGRADE_PATCH_PATH=$TEMP_PATH/$UPGRADE_PATCH
 
 # 驱动源码路径
 export SRC_PATH=$CONTEXT_PATH/src
@@ -29,72 +33,64 @@ case $1 in
 
 # 从Github下载交叉编译工具和源码
 download)
-	mkdir .cache \
-	&& git clone --depth=1 https://github.com/raspberrypi/tools.git .cache/tools \
-	&& git clone -b $KERNEL_VERSION --depth=1 https://github.com/raspberrypi/linux.git ./cache/$KERNEL_VERSION
+	mkdir .$CACHE_PATH \
+	&& git clone --depth=1 https://github.com/raspberrypi/tools.git $CACHE_PATH/tools \
+	&& git clone -b $KERNEL_VERSION --depth=1 https://github.com/raspberrypi/linux.git $CACHE_PATH/linux-$KERNEL_VERSION
 	;;
 
 # 从Gitee下载交叉编译工具和源码
 download-gitee)
-	mkdir .cache \
-	git clone --depth=1 https://gitee.com/rpi-image/tools.git $TOOLS_PATH \
-	&& git clone -b $KERNEL_VERSION --depth=1 https://gitee.com/rpi-image/linux.git $LIUNX_PATH
+	mkdir $CACHE_PATH \
+	git clone --depth=1 https://gitee.com/rpi-image/tools.git $CACHE_PATH/tools \
+	&& git clone -b $KERNEL_VERSION --depth=1 https://gitee.com/rpi-image/linux.git $CACHE_PATH/linux-$KERNEL_VERSION
 	;;
 
 # 净化目录
 purge)
 	rm -rf $UPGRADE_PATCH_PATH* \
-	&& rm -rf ./temp \
-	&& rm -rf .cache
+	&& rm -rf $CACHE_PATH \
+	&& rm -rf $TEMP_PATH
 	;;
 
 # 清除编译
 clean)
 	rm -rf $UPGRADE_PATCH_PATH* \
-	&& rm -rf ./temp
+	&& rm -rf $TEMP_PATH
 	;;
 
 # 编译
 build)
-
-	export PATH=$TOOLS_PATH/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin:$PATH \
-	
-	&& mkdir $CONTEXT_PATH/temp \
-
-	&& cp -r .cache/$KERNEL_VERSION $LINUX_PATH \
-	&& cp -r .cache/tools $LINUX_PATH \
-
+	mkdir $TEMP_PATH \
+	&& cp -r $CACHE_PATH/linux-$KERNEL_VERSION $LINUX_PATH \
+	&& cp -r $CACHE_PATH/tools $TOOLS_PATH \
+	&& export PATH=$TOOLS_PATH/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin:$PATH \
 	&& cp $SRC_PATH/public/simcom_wwan.c $LINUX_PATH/drivers/net/usb \
 	&& cp $SRC_PATH/$KERNEL_VERSION/option.c $LINUX_PATH/drivers/usb/serial/option.c \
 	&& cp $SRC_PATH/$KERNEL_VERSION/bcm2709_defconfig $LINUX_PATH/arch/arm/configs/bcm2709_defconfig \
 	&& echo "\nobj-\$(CONFIG_USB_USBNET) += usbnet.o simcom_wwan.o\n" >> $LINUX_PATH/drivers/net/usb/Makefile \
-	
 	&& cd $LINUX_PATH \
-	
 	&& make mrproper \
 	&& make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcm2709_defconfig \
 	&& make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j $CONFIG_SPEED_UP zImage modules dtbs \
-	
 	&& mkdir $UPGRADE_PATCH_PATH \
 	&& make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- modules_install INSTALL_MOD_PATH=$UPGRADE_PATCH_PATH \
 	&& scripts/mkknlimg arch/arm/boot/zImage $UPGRADE_PATCH_PATH/kernel7.img \
-	
 	&& cp -r $LINUX_PATH/arch/arm/boot/dts $UPGRADE_PATCH_PATH/dts \
-	
 	&& cd $CONTEXT_PATH \
-	
 	&& cp -r $SRC_PATH/public/upgrade.sh $UPGRADE_PATCH_PATH/ \
-	
 	&& tar -zcf UpgradePatch.tar.gz $UPGRADE_PATCH/
 	;;
 
 *)
+	echo ""
 	echo "如何使用: ./configure.sh [command]"
-	echo "command: download           从Github下载交叉编译工具和源码"
-	echo "command: download-gitee     从Gitee下载交叉编译工具和源码"
-	echo "command: purge              净化目录"
-	echo "command: clean              清除编译"
-	echo "command: build              编译"
+	echo ""
+	echo "    command: download           从Github下载交叉编译工具和源码"
+	echo "    command: download-gitee     从Gitee下载交叉编译工具和源码"
+	echo "    command: purge              净化目录"
+	echo "    command: clean              清除编译"
+	echo "    command: build              编译"
+	echo ""
 	;;
 
 esac
